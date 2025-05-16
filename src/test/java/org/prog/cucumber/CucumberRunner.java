7849e5e3e72007b8fa6bc8dd47c19e6fc61360c7
aosipenko <aoskiev@gmail.com>
2025-05-16 18:19:38 +0200

    Session 12: SQL setup

--- src/test/java/org/prog/cucumber/CucumberRunner.java
+++ src/test/java/org/prog/cucumber/CucumberRunner.java
@@ -3,7 +3,6 @@ package org.prog.cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.prog.cucumber.steps.WebSteps;
 import org.prog.page.CloudFlarePage;
 import org.prog.page.GooglePage;
@@ -12,6 +11,9 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 import java.net.MalformedURLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @CucumberOptions(
         tags = "@wip",
@@ -21,17 +23,31 @@ import java.net.MalformedURLException;
 public class CucumberRunner extends AbstractTestNGCucumberTests {
 
     private WebDriver driver;
+    private Connection connection;
 
     @BeforeSuite
-    public void beforeSuite() throws MalformedURLException {
+    public void beforeSuite() throws MalformedURLException, SQLException {
         String driverType = System.getProperty("driver.type", "jenkins");
+        String sqlHost;
+        if (driverType.equals("jenkins")) {
+            sqlHost = "jdbc:mysql://mysql-db-1:3306/db";
+        } else {
+            sqlHost = "jdbc:mysql://localhost:3306/db";
+        }
+
         driver = WebDriverFactory.getDriver(driverType);
+        connection = DriverManager.getConnection(sqlHost, "root", "password");
         WebSteps.googlePage = new GooglePage(driver);
         WebSteps.cloudFlarePage = new CloudFlarePage(driver);
     }
 
     @AfterSuite
-    public void tearDown() {
-        driver.quit();
+    public void tearDown() throws SQLException {
+        if (driver != null) {
+            driver.quit();
+        }
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
     }
 }
--- src/test/java/org/prog/cucumber/steps/SQLSteps.java
+++ src/test/java/org/prog/cucumber/steps/SQLSteps.java
@@ -11,107 +11,74 @@ import java.util.List;
 public class SQLSteps {
 
     public static String randomPersonFirstLastName;
+    public static Connection connection;
 
     @Given("I pick random person from DB")
     public void iPickRandomPersonFromDB() throws SQLException {
         Connection connection = null;
-        try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/db",
-                    "root", "password");
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM Persons ORDER BY RAND() LIMIT 1");
-            if (resultSet.next()) {
-                randomPersonFirstLastName = resultSet.getString("FirstName") + " " +
-                        resultSet.getString("LastName");
-            } else {
-                Assertions.fail("No results found");
-            }
-        } finally {
-            if (connection != null) {
-                connection.close();
-            }
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM Persons ORDER BY RAND() LIMIT 1");
+        if (resultSet.next()) {
+            randomPersonFirstLastName = resultSet.getString("FirstName") + " " +
+                    resultSet.getString("LastName");
+        } else {
+            Assertions.fail("No results found");
         }
     }
 
     @Given("I pick random person from DB as {string}")
     public void iPickRandomPersonFromDB(String alias) throws SQLException {
         Connection connection = null;
-        try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/db",
-                    "root", "password");
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM Persons ORDER BY RAND() LIMIT 1");
-            if (resultSet.next()) {
-                DataHolder.HOLDER.put(alias, resultSet.getString("FirstName") + " " +
-                        resultSet.getString("LastName"));
-            } else {
-                Assertions.fail("No results found");
-            }
-        } finally {
-            if (connection != null) {
-                connection.close();
-            }
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM Persons ORDER BY RAND() LIMIT 1");
+        if (resultSet.next()) {
+            DataHolder.HOLDER.put(alias, resultSet.getString("FirstName") + " " +
+                    resultSet.getString("LastName"));
+        } else {
+            Assertions.fail("No results found");
         }
     }
 
     @Given("I store {string} id DB")
     public void iStoreIdDB(String alias) throws SQLException {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/db",
-                    "root", "password");
-            PreparedStatement statement = connection.prepareStatement(
-                    "INSERT INTO Persons (FirstName, LastName, Title, Gender, Nat) VALUES (?,?,?,?,?)");
+        PreparedStatement statement = connection.prepareStatement(
+                "INSERT INTO Persons (FirstName, LastName, Title, Gender, Nat) VALUES (?,?,?,?,?)");
 
-            //TODO: find a way to transfer data from RestSteps
-            List<PersonDto> clients = (List<PersonDto>) DataHolder.HOLDER.get(alias);
+        //TODO: find a way to transfer data from RestSteps
+        List<PersonDto> clients = (List<PersonDto>) DataHolder.HOLDER.get(alias);
 
-            for (PersonDto client : clients) {
-                statement.setString(1, client.getName().getFirst());
-                statement.setString(2, client.getName().getLast());
-                statement.setString(3, client.getName().getTitle());
-                statement.setString(4, client.getGender());
-                statement.setString(5, client.getNat());
-                try {
-                    statement.execute();
-                } catch (SQLException e) {
-                    System.out.println("Failed to store client " + client);
-                }
-            }
-        } finally {
-            if (connection != null) {
-                connection.close();
+        for (PersonDto client : clients) {
+            statement.setString(1, client.getName().getFirst());
+            statement.setString(2, client.getName().getLast());
+            statement.setString(3, client.getName().getTitle());
+            statement.setString(4, client.getGender());
+            statement.setString(5, client.getNat());
+            try {
+                statement.execute();
+            } catch (SQLException e) {
+                System.out.println("Failed to store client " + client);
             }
         }
     }
 
     @Given("I store previously generated people id DB")
     public void iStorePreviouslyGeneratedPeopleIdDB() throws SQLException {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/db",
-                    "root", "password");
-            PreparedStatement statement = connection.prepareStatement(
-                    "INSERT INTO Persons (FirstName, LastName, Title, Gender, Nat) VALUES (?,?,?,?,?)");
+        PreparedStatement statement = connection.prepareStatement(
+                "INSERT INTO Persons (FirstName, LastName, Title, Gender, Nat) VALUES (?,?,?,?,?)");
 
-            //TODO: find a way to transfer data from RestSteps
-            List<PersonDto> clients = RestSteps.randomPersons;
+        //TODO: find a way to transfer data from RestSteps
+        List<PersonDto> clients = RestSteps.randomPersons;
 
-            for (PersonDto client : clients) {
-                statement.setString(1, client.getName().getFirst());
-                statement.setString(2, client.getName().getLast());
-                statement.setString(3, client.getName().getTitle());
-                statement.setString(4, client.getGender());
-                statement.setString(5, client.getNat());
-                try {
-                    statement.execute();
-                } catch (SQLException e) {
-                    System.out.println("Failed to store client " + client);
-                }
-            }
-        } finally {
-            if (connection != null) {
-                connection.close();
+        for (PersonDto client : clients) {
+            statement.setString(1, client.getName().getFirst());
+            statement.setString(2, client.getName().getLast());
+            statement.setString(3, client.getName().getTitle());
+            statement.setString(4, client.getGender());
+            statement.setString(5, client.getNat());
+            try {
+                statement.execute();
+            } catch (SQLException e) {
+                System.out.println("Failed to store client " + client);
             }
         }
     }
