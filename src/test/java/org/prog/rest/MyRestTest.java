fd2a1ac7b0e8744282f97c8a124aa2128f9120e9
o-khimichenko <mr.lexus.x@gmail.com>
2025-06-05 23:24:35 +0200

    Session8 HW

--- src/test/java/org/prog/rest/MyRestTest.java
+++ src/test/java/org/prog/rest/MyRestTest.java
@@ -13,9 +13,9 @@ import org.testng.annotations.Test;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.oneOf;
 
-//TODO: add location to request query
-//TODO: write assertion that confirms city is not null
-//TODO: write assertion that confirms coordintaes are decimal digits (number is . in it: 123.123 or -123.123 etc)
+//TODO: add location to request query +
+//TODO: write assertion that confirms city is not null +
+//TODO: write assertion that confirms coordintaes are decimal digits (number is . in it: 123.123 or -123.123 etc) +
 
 public class MyRestTest {
 
@@ -25,7 +25,7 @@ public class MyRestTest {
         request.baseUri("https://randomuser.me");
         request.basePath("/api/");
         request.header("Content-Type", "application/json");
-        request.param("inc", "gender,name,nat");
+        request.param("inc", "gender,name,nat,location");
         request.param("noinfo");
 
         Response response = request.get();
@@ -39,5 +39,7 @@ public class MyRestTest {
         validatableResponse.statusCode(200);
         validatableResponse.header("Content-Type", equalTo("application/json; charset=utf-8"));
         validatableResponse.body("results[0].gender", equalTo("female"));
+        validatableResponse.body("coordinates", Matchers.matchesRegex("-?\\d+\\.\\d+"));
+        validatableResponse.body("city", Matchers.notNullValue());
     }
 }
